5bf738568c2417cf3252b93e4b3d036bd1cf8fc6
liudaojie <liudaojie@yy.com>
2020-12-16 00:41:27 +0800

    feat: format

--- test/case/base/src/entry/index/index.jsx
+++ test/case/base/src/entry/index/index.jsx
@@ -4,10 +4,12 @@ import {} from '../../../../../../'
 import logoIcon from './images/logo.png'
 import './index.css'
 
-const App = <div className='dm-box'>
-  <img src={logoIcon} />
-  <h1 className='dm-title'>hello plugin </h1>
-  <p className='dm-info'>demo only</p>
-</div>
+const App = (
+  <div className='dm-box'>
+    <img src={logoIcon} />
+    <h1 className='dm-title'>hello plugin </h1>
+    <p className='dm-info'>demo only</p>
+  </div>
+)
 
 ReactDOM.render(App, document.querySelector('#app'))
